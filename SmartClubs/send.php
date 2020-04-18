195c10e10b414f4efb201240dfe8b4ce33fdb4a2
Ilya Zotov <zie2000@mail.ru>
2020-04-19 02:21:17 +0300

    update

--- SmartClubs/send.php
+++ SmartClubs/send.php
@@ -1,11 +1,17 @@
 <?php
 $fio = $_POST['fio'];
+$spisok = $_POST['spisok'];
 $telefon = $_POST['telefon'];
 $fio = htmlspecialchars($fio);
+$spisok = htmlspecialchars($spisok);
 $telefon = htmlspecialchars($telefon);
 $fio = urldecode($fio);
+$spisok = urldecode($spisok);
 $telefon = urldecode($telefon);
 $fio = trim($fio);
+$spisok = trim($spisok);
 $telefon = trim($telefon);
- (mail("zie2000@mail.ru", "Заявка с сайта", "ФИО:".$fio.". telefon: ".$telefon."From: zie2000@mail.ru \r\n"))
+(mail("zie2000@mail.ru", "Заявка с сайта", "ФИО:".$fio.". телефон: ".$telefon. ".секция: ".$spisok."From: zie2000@mail.ru \r\n"))
+
+
 ?>
